4717c5e7baf34e2b7aa470c6d83fcec8417b8f3f
kalininilya <kalinination@gmail.com>
2015-12-16 06:34:54 +0300

    Update kr2.m

--- kr2.m
+++ kr2.m
@@ -1,4 +1,3 @@
-%solve(, sqr(x+2) + 0.5*sqr(y) -1)
 clear
 clc
 
@@ -12,7 +11,4 @@ x0 = double(R.x)
 y0 = double(R.y)
 
 plot (x0, y0, 'or')
-
-%y1 = fzero('(x+2)^2 + 0.5*(1/x - cos(x))^2-1', 0)
-%x1 = -2 + sqrt(1-0.5*y^2)
 
